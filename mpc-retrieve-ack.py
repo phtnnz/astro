ee289e5fed968859f9bda974146618114057f81e
phtnnz <git16@mj.m-j-s.net>
2024-06-26 17:15:35 +0200

    Removed report file handling

--- mpc-retrieve-ack.py
+++ mpc-retrieve-ack.py
@@ -34,6 +34,9 @@
 #       Added -D --sort-by-date option for overview output
 # Version 1.4 / 2024-03-20
 #       Somewhat refactored, using jsonconfig module
+# Version 1.5 / 2024-06-26
+#       Refactored, removed all code for reading report txt files, this will be
+#       handled in mpc-retrieve-reports
 
 import sys
 import os
@@ -59,7 +62,7 @@ from mpc.mpcdata80    import MPCData80
 
 
 NAME    = "mpc-retrieve-ack"
-VERSION = "1.4 / 2024-03-20"
+VERSION = "1.5 / 2024-06-26"
 AUTHOR  = "Martin Junius"
 
 CONFIG = "imap-account.json"
@@ -75,8 +78,6 @@ class Options:
     list_msgs   = False     # -L --list-messages-only
     inbox       = "INBOX"   # -F --imap-folder
     msgs_list   = None      # -m --msgs
-    mpc1992     = False     # -M --mpc1992-reports
-    ades        = False     # -A --ades-reports
     output      = None      # -o --output
     csv         = False     # -C --csv
     overview    = False     # -O --overview
@@ -488,173 +489,6 @@ def retrieve_from_mpc_mpec(id):
 
 
 
-def retrieve_from_directory(root):
-    for dir, subdirs, files in os.walk(root):
-        verbose("Processing directory", dir)
-        for f in files:
-            if f.endswith(".txt") or f.endswith(".TXT"):
-                # print("f =", f)
-                process_file(os.path.join(dir, f))
-
-
-
-def process_file(file):
-    with open(file, "r") as fh:
-        line1 = fh.readline().strip()
-        obj = None
-
-        # Old MPC 1992 report format
-        if line1.startswith("COD "):
-            if Options.mpc1992:
-                verbose("Processing MPC1992", file)
-                format = "MPC1992"
-                obj = process_mpc1992(fh, line1)
-
-        # New ADES (PSV) report format
-        elif line1 == "# version=2017":
-            if Options.ades:
-                verbose("Processing ADES", file)
-                format = "ADES"
-                obj = process_ades(fh, line1)
-
-        else:
-            verbose("Not processing", file)
-
-        if obj:
-            obj["_file"] = file.replace("\\", "/")
-            obj["_format"] = format
-            JSONOutput.add_json_obj(obj)
-
-
-
-def process_mpc1992(fh, line1):
-    mpc1992_obj = {}
-    mpc1992_obj["_observations"] = []
-    ids = {}
-
-    line = line1
-    while line:
-        # meta data header
-        m = re.match(r'^([A-Z0-9]{3}) (.+)$', line)
-        if m:
-            ic(m.groups())
-            (m1, m2) = m.groups()
-            # requires Python >= 3.10!
-            match m1:
-                case "COD":
-                    mpc1992_obj["observatory"] = {"mpcCode": m2}
-                case "CON":
-                    mpc1992_obj["submitter"] = {"name": m2}
-                case "OBS":
-                    mpc1992_obj["observers"] = {"name": m2}
-                case "MEA":
-                    mpc1992_obj["measurers"] = {"name": m2}
-                case "TEL":
-                    ## ADES
-                    # # telescope
-                    # ! design reflector
-                    # ! aperture 0.25
-                    # ! fRatio 4.5
-                    # ! detector CMO
-                    ## MPC1992
-                    # TEL 0.25-m f/4.5 reflector + CMO
-                    # sometimes f/X is missing
-                    ic(m2)
-                    mtel = re.match(r'^([0-9.]+)-m f/([0-9.]+) (.+) \+ (.+)$', m2)
-                    if not mtel:
-                        mtel = re.match(r'^([0-9.]+)-m ()(.+) \+ (.+)$', m2)
-                    if mtel:
-                        ic(mtel.groups())
-                        mpc1992_obj["telescope"] = {"aperture": mtel.group(1), 
-                                                    "fRatio":   mtel.group(2),
-                                                    "design":   mtel.group(3), 
-                                                    "detector": mtel.group(4)}
-                    else:
-                        mpc1992_obj["telescope"] = {"_description": m2}      ##FIXME: split as in ADES report
-                case "NUM":
-                    mpc1992_obj["_number"] = int(m2)
-                case "ACK":
-                    mpc1992_obj["_ack_line"] = m2
-                case "AC2":
-                    mpc1992_obj["_ac2_line"] = m2
-                case "COM":
-                    mpc1992_obj["_comment"] = m2
-                case "NET":
-                    mpc1992_obj["_catalog"] = m2
-
-        # data lines
-        else:
-            data = MPCData80(line)
-            mpc1992_obj["_observations"].append(data.get_obj())
-            ids[line] = True
-
-        line = fh.readline().rstrip()
-        if line.startswith("----- end"):
-            break
-
-    wamo = retrieve_from_mpc_wamo(ids)
-    if wamo:
-        mpc1992_obj["_wamo"] = wamo
-    verbose("JSON =", json.dumps(mpc1992_obj, indent=4))
-
-    return mpc1992_obj
-
-
-
-def dict_remove_ws(dict):
-    """ Remove white space for dict keys and values """
-    return {k.strip():v.strip() for k, v in dict.items()}
-
-
-def process_ades(fh, line1):
-    ades_obj = {}
-
-    # Read report txt file
-    key1 = None
-    key2 = None
-    while True:
-        pos = fh.tell()
-        line = fh.readline()
-        if not line:
-            break
-
-        # meta data header
-        m = re.match(r'^(#|!) (\w+) ?(.+)?$', line.strip())
-        if m:
-            ic(m.groups())
-            (m1, m2, m3) = m.groups()
-            if m1 == "#":
-                key1 =  m2
-                ades_obj[key1] ={}
-            if m1 == "!":
-                key2 = m2
-                ades_obj[key1][key2] = m3
-
-        # PSV from this line on
-        else:
-            fh.seek(pos)
-            ades_obj["_observations"] = []
-            reader = csv.DictReader(fh, delimiter='|', quoting=csv.QUOTE_NONE)
-            for row in reader:
-                row1 = dict_remove_ws(row)
-                ic(row1)
-                ades_obj["_observations"].append(row1)
-            break
-
-    # Get trackIds and mpcCode to query WAMO
-    ids = {}
-    for trk in ades_obj["_observations"]:
-        ids[trk["trkSub"] + " " + trk["stn"]] = True
-    ades_obj["_ids"] = ids
-    wamo = retrieve_from_mpc_wamo(ids)
-    if wamo:
-        ades_obj["_wamo"] = wamo
-    verbose("JSON =", json.dumps(ades_obj, indent=4))
-
-    return ades_obj
-
-
-
 # Hack from https://stackoverflow.com/questions/6405208/how-to-convert-numeric-string-ranges-to-a-list-in-python
 def str_to_list(s):
     return sum(((list(range(*[int(j) + k for k,j in enumerate(i.split('-'))]))
@@ -668,7 +502,7 @@ def main():
 
     arg = argparse.ArgumentParser(
         prog        = NAME,
-        description = "Retrieve MPC ACK data",
+        description = "Retrieve MPC ACK mails",
         epilog      = "Version " + VERSION + " / " + AUTHOR)
     arg.add_argument("-v", "--verbose", action="store_true", help="verbose messages")
     arg.add_argument("-d", "--debug", action="store_true", help="more debug messages")
@@ -677,9 +511,6 @@ def main():
     arg.add_argument("-f", "--imap-folder", help="IMAP folder(s) (comma-separated) to retrieve mails from, default "+Options.inbox)
     arg.add_argument("-L", "--list-messages-only", action="store_true", help="list messages in IMAP folder only")
     arg.add_argument("-m", "--msgs", help="retrieve messages in MSGS range only, e.g. \"1-3,5\", default all")
-    arg.add_argument("directory", nargs="*", help="read MPC reports from directory/file instead of ACK mails")
-    arg.add_argument("-M", "--mpc1992-reports", action="store_true", help="read old MPC 1992 reports")
-    arg.add_argument("-A", "--ades-reports", action="store_true", help="read new ADES (PSV format) reports")
     arg.add_argument("-o", "--output", help="write JSON/CSV to OUTPUT file")
     arg.add_argument("-C", "--csv", action="store_true", help="use CSV output format (instead of JSON)")
     arg.add_argument("-O", "--overview", action="store_true", help="create overview of objects and observations")
@@ -698,26 +529,12 @@ def main():
         Options.inbox = args.imap_folder
     if args.msgs:
         Options.msgs_list = str_to_list(args.msgs)
-    Options.mpc1992     = args.mpc1992_reports
-    Options.ades        = args.ades_reports
     Options.output      = args.output
     Options.csv         = args.csv
     Options.overview    = args.overview
     Options.sort_by_date= args.sort_by_date
 
-    if args.directory:
-        for dir in args.directory:
-            # quick hack: Windows PowerShell adds a stray " to the end of dirname 
-            # if it ends with a backslash \ AND contains a space!!!
-            # see here https://bugs.python.org/issue39845
-            dir = dir.rstrip("\"")
-            if os.path.isdir(dir):
-                retrieve_from_directory(dir)
-            if os.path.isfile(dir):
-                process_file(dir)
-    else:
-        retrieve_from_imap(config)
-
+    retrieve_from_imap(config)
 
     if Options.overview and not Options.output:
         ObsOverview.print_all()
