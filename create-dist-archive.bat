383a3b5dbef05291fe3df75278ffd8b9d38ee65d
phtnnz <git16@mj.m-j-s.net>
2024-08-26 22:52:41 +0200

    Delete old one before creating new archive, recursively exclude __pycache__

--- create-dist-archive.bat
+++ create-dist-archive.bat
@@ -1,3 +1,4 @@
 @echo off
-echo Creating distribution archive ...
-"C:\Program Files\7-Zip\7z.exe" a -t7z -spf -x!__pycache__ IAS-astro-python.7z @files.dist
+echo Creating new distribution archive ...
+del IAS-astro-python.7z
+"C:\Program Files\7-Zip\7z.exe" a -t7z -spf -xr!__pycache__ IAS-astro-python.7z @files.dist
