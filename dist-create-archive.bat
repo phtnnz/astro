e2f45797f3d6eb3ab6d65880facf48400d0d51cc
phtnnz <git16@mj.m-j-s.net>
2025-10-21 21:38:15 +0200

    Create new dist archive IAS-templates.7z

--- dist-create-archive.bat
+++ dist-create-archive.bat
@@ -1,6 +1,12 @@
 @echo off
+set DISTARC=dist\IAS-templates.7z
+set DISTLIST=dist\files1.dist
+echo Creating new distribution archive %DISTARC% ...
+del %DISTARC%
+"C:\Program Files\7-Zip\7z.exe" a -t7z -spf -xr!__pycache__ %DISTARC% @%DISTLIST%
+
 set DISTARC=dist\IAS-astro-python.7z
-set DISTLIST=dist\files.dist
+set DISTLIST=dist\files2.dist
 echo Creating new distribution archive %DISTARC% ...
 del %DISTARC%
 "C:\Program Files\7-Zip\7z.exe" a -t7z -spf -xr!__pycache__ %DISTARC% @%DISTLIST%
